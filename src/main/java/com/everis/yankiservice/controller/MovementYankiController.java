245bfd495730f2e73f620c81628432a30702cea3
angelflores124 <ajoelflores124@gmail.com>
2021-07-14 15:50:48 -0500

    yanki update files

--- src/main/java/com/everis/yankiservice/controller/MovementYankiController.java
+++ src/main/java/com/everis/yankiservice/controller/MovementYankiController.java
@@ -5,11 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.everis.yankiservice.model.Customer;
 import com.everis.yankiservice.model.Movement;
 import com.everis.yankiservice.topic.producer.YankiServiceProducer;
-
 import reactor.core.publisher.Mono;
 
 @RestController
